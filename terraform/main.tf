f1164ebfa190e17631ec108fc521918f37a21fc5
amandaleandro <amandaleandrosoares@gmail.com>
2024-12-13 17:28:23 -0300

    Ajustando nova versao

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,63 @@
-# Provider AWS
 provider "aws" {
   region = "us-east-1"
 }
 
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  # Remova a versão ou defina a versão mais recente.
-  name                 = "eks-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = ["us-east-1a", "us-east-1b"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  version = "3.5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+# Criar Role para EKS
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
 }
 
 
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.21"
-  vpc_id          = module.vpc.vpc_id
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.22"
   subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  
+}
+
+resource "aws_ecr_repository" "helloworld_repo" {
+  name                 = "helloworld"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
